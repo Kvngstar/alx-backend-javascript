07fbeef3dcff338a154969021d26916f9de500f8
Kingsley Kelechukwu Okoronkwo <kvngsley019@gmail.com>
2024-10-07 21:51:49 +0000

    third review: reviewed 10-loop.js file (code-issue)

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,5 +1,5 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let idx of array) {
+  for (let idx of array.keys()) {
     const value = array[idx];
     array[idx] = appendString + value;
   }
