6e490406c4ed23f3e20e4bc4b1de6d3126e0dcad
Kingsley Kelechukwu Okoronkwo <kvngsley019@gmail.com>
2024-10-07 20:41:31 +0000

    second review: reviewed 8-getBudgetCurrentYear.js file (code-issue)

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -5,11 +5,9 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-    budget[`income-${getCurrentYear()}`]: income;
-    budget[`gdp-${getCurrentYear()}`]: gdp;
-    budget[`capita-${getCurrentYear()}`]: capita;
-
-
+    [`income-${getCurrentYear()}`]: income;
+    [`gdp-${getCurrentYear()}`]: gdp;
+    [`capita-${getCurrentYear()}`]: capita;
   };
 
   return budget;
