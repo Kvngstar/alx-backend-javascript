2e41f469e8e26dd732250c7b0116b90e453f0e47
Kingsley Kelechukwu Okoronkwo <kvngsley019@gmail.com>
2024-10-08 10:53:45 +0000

    first review: review 12-createReportObject.js  file (code-isssue)

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList){
+  const departmentProps = Object.keys(employeesList);
+
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments: () => departmentProps.length,
+  }
+}
