ecbad88df91f1c617f1fff98800dd60b415af96b
Kingsley Kelechukwu Okoronkwo <kvngsley019@gmail.com>
2024-10-08 15:48:23 +0000

    fourth review: review 101-iterateThroughObject.js file (lint-issue)

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
+  //  eslint-disable
   const iteratorLength = reportWithIterator.length;
   let stringConstruct = '';
   let counter = 0;
-  //  eslint-disable
+
   for (const value of reportWithIterator) {
     if (counter === iteratorLength - 1) {
       stringConstruct = stringConstruct + value;
