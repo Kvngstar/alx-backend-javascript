da5b1e926ced4817865c05740c3034ad3e6bfee5
Kingsley Kelechukwu Okoronkwo <kvngsley019@gmail.com>
2024-10-08 15:37:50 +0000

    first review: review 101-iterateThroughObject.js file (code-issue)

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,16 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const iteratorLength = reportWithIterator.length;
+  let stringConstruct = '';
+  let counter = 0;
+
+  for (const value of reportWithIterator) {
+    if (counter === iteratorLength - 1) {
+      stringConstruct = stringConstruct + value;
+    } else {
+      stringConstruct = stringConstruct + value + ' | ';
+    }
+    counter += 1;
+  }
+
+  return stringConstruct;
+}
