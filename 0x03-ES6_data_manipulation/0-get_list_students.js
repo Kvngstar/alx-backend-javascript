c2b4974b680610690d5eaaef65d20e7ec84067da
kvngstar <kvngsley019@gmail.com>
2024-10-09 00:00:46 +0000

    second attempt: 1-get_list_student_ids.js (lint-issue)

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,7 +4,7 @@ export default function getListStudents() {
     firstName: 'Guillaume',
     location: 'San Francisco',
   },
-  {id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
