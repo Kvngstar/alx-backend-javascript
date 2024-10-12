54ce78f02db5a066fb42b2245a3b8e95160b65d2
kvngstar <kvngsley019@gmail.com>
2024-10-12 11:25:51 +0000

    second review: 3-get_ids_sum.js file (lint-issue)

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(students) {
-  return students.reduce((agg, student) => agg + student.id, 0);
+  return students.reduce((agg, current) => agg + current), 0);
 }
