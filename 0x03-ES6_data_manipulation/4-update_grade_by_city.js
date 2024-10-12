a6438742e9d674d6776d6931adc1075c5e86cda1
kvngstar <kvngsley019@gmail.com>
2024-10-12 11:48:31 +0000

    first review: 4-update_grade_by_city.js (lint&code-issue)

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -8,17 +8,17 @@ export default function updateStudentGradeByCity(
   );
 
   const gradeStudents = filterStudents.map(
-    (student) => {
-      for (const gradeInfo of newGrades) {
-        if (student.id === gradeInfo.studentId) {
-          student.grade = gradeInfo.grade; // eslint-disable-line no-param-reassign
-        }
+  (student) => {
+    for (const gradeInfo of newGrades) {
+      if (student.id === gradeInfo.studentId) {
+        student.grade = gradeInfo.grade; // eslint-disable-line no-param-reassign
       }
-      if (student.grade === undefined) {
-        student.grade = 'N/A'; // eslint-disable-line no-param-reassign
-      }
-      return student;
-    },
+    }
+    if (student.grade === undefined || !student.grade) {
+      student.grade = 'N/A'; // eslint-disable-line no-param-reassign
+    }
+    return student;
+  },
   );
 
   return gradeStudents;
