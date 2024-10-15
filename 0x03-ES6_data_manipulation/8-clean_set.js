d7323ca310077b9ab6ddcb4d8ea2fd7b4230c8ef
kvngstar <kvngsley019@gmail.com>
2024-10-15 07:29:35 +0000

    sixth review: 8-clean_set.js (code+lint)

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,6 @@ export default function cleanSet(set, startString) {
   const strlen = startString ? startString.length : 0;
   let formNewString = '';
   // eslint-disable-next-line no-unused-expressions
-  set.forEach((elem) => { (startString && typeof startString === 'string' && elem.startsWith(startString)) ? (formNewString = (formNewString + ((!formNewString) ? '' : '-') + elem.slice(strlen))) : ''; });
+  set.forEach((elem) => { (startString && set && typeof startString === 'string' && elem.startsWith(startString)) ? (formNewString = (formNewString + ((!formNewString) ? '' : '-') + elem.slice(strlen))) : ''; });
   return formNewString;
 }
