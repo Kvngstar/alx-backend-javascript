6e2b5c799cf68f0e8497db694323532d7a7896f1
kvngstar <kvngsley019@gmail.com>
2025-02-05 00:23:21 +0100

    fix: removed multiple line

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -6,7 +6,6 @@ const HOST = 'localhost';
 const app = http.createServer();
 const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
-
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {
     reject(new Error('Cannot load the database'));
